e41ddec03bce362699d194ba02176d2852514979
poyu <weairch@hotmail.com>
2021-01-05 23:45:53 +0800

    test facebook butoon

--- public/js/signin.js
+++ public/js/signin.js
@@ -77,8 +77,6 @@ function testAPI() {                      // Testing Graph API after login.  See
     console.log("Welcome!  Fetching your information.... ");
     FB.api("/me", function(response) {
         console.log("Successful login for: " + response.name);
-        document.getElementById("status").innerHTML =
-        "Thanks for logging in, " + response.name + "!";
     });
 }
 
